0cd4418f968ff85537aeb2779238f3afb089b586
z077514 <daniel.c.parker@target.com>
2014-09-09 08:39:42 -0500

    initial commit

---
+++ recipes/solr.rb
@@ -0,0 +1,7 @@
+node.default['cassandra']['solr'] = true
+#Solr needs vnodes disabled
+node.default['cassandra']['num_tokens']= '1'
+#This is set because solr likes to warn a lot
+node.default['cassandra']['log_level'] = "ERROR"
+
+include_recipe "dse::default"
