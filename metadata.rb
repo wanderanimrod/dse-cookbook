0075976657160dc7068ba99df12620f59048e88a
Bill Warner <bill.warner@gmail.com>
2014-10-21 08:32:17 -0700

    version bump for feature add (role based seeds)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "daniel.c.parker@target.com"
 license          "Apache 2.0"
 description      "Installs/Configures Datastax Enterprise."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.11"
+version          "3.0.12"
 
 depends "java", "~> 1.14"
 depends "yum", "~> 2.3"
