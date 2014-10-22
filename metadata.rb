1c63e24df390d394538601d8603b884f7990f2c3
z077514 <daniel.c.parker@target.com>
2014-10-22 10:16:49 -0500

    adding supports to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,12 @@ description      "Installs/Configures Datastax Enterprise."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "3.0.12"
 
+%w(redhat centos).each do |name|
+  supports name, '~> 6.4'
+end
+
+supports 'ubuntu', '= 14.04'
+
 depends "java", "~> 1.14"
 depends "yum", "~> 2.3"
 depends "apt", "~> 2.0"
