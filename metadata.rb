7e131bc373d2f13e92b6479d4004a49b5fb581be
z077514 <daniel.c.parker@target.com>
2014-10-28 14:23:56 -0500

    slight change to support

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "3.0.13"
 
 %w(redhat centos).each do |name|
-  supports name, '~> 6.4'
+  supports name, '>= 6.4'
 end
 
 supports 'ubuntu', '= 14.04'
