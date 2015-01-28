4c3e60de42656fe57a678aba3b06f93110c63630
Bill Warner <bwarner@demark.com>
2015-01-28 08:33:12 -0700

    reverse notifaction/subscribes to insure an install can be done without starting the service

--- recipes/service.rb
+++ recipes/service.rb
@@ -5,4 +5,11 @@ service node['cassandra']['dse']['service_name'] do
   action [:enable, :start]
   subscribes :restart, 'java_ark[jdk]'
   subscribes :restart, 'package[dse-full]'
+  subscribes :restart, 'template[/etc/default/dse]'
+  subscribes :restart, "template[#{node['cassandra']['audit_dir']}/log4j-server.properties]"
+  subscribes :restart, "template[#{node['cassandra']['dse']['conf_dir']}/dse.yaml]"
+  subscribes :restart, "template[#{node['cassandra']['dse']['conf_dir']}/cassandra/cassandra.yaml]"
+  subscribes :restart, "template[#{node['cassandra']['dse']['conf_dir']}/cassandra/cassandra-env.sh]"
+  subscribes :restart, "template[#{node['cassandra']['dse']['conf_dir']}/cassandra/cassandra-rackdc.properties]"
+  subscribes :restart, "template[#{node['cassandra']['dse']['conf_dir']}/cassandra/cassandra-topology.properties]"
 end
