2e196e5675b4b1d0f93ab6822cc185db92eb1e50
Bill Warner <bwarner@demark.com>
2015-03-12 07:39:15 -0700

    fix spelling typos

--- test/integration/default/serverspec/localhost/dse_spec.rb
+++ test/integration/default/serverspec/localhost/dse_spec.rb
@@ -6,19 +6,19 @@ describe service('dse') do
 end
 
 describe 'DSE Cassandra running' do
-  it 'is listening on prot 9042 for cassandra native traffic' do
+  it 'is listening on port 9042 for cassandra native traffic' do
     expect(port(9042)).to be_listening
   end
 
-  it 'is listenint on prot 9160 for cassandra thrift traffic' do
+  it 'is listening on port 9160 for cassandra thrift traffic' do
     expect(port(9160)).to be_listening
   end
 
-  it 'is listenint on prot 7000 for intranode traffic' do
+  it 'is listening on port 7000 for intranode traffic' do
     expect(port(7000)).to be_listening
   end
 
-  it 'is listenint on prot 7199 for jmx traffic' do
+  it 'is listening on port 7199 for jmx traffic' do
     expect(port(7199)).to be_listening
   end
 end
