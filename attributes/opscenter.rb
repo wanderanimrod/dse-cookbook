982da9f83211edbabbf08b9341609edebf1d8e10
Michael Belt <michael@mdbelt.com>
2015-11-18 14:43:36 -0600

    Added LDAP attributes for DSE Cassandra & Opscenter.

--- attributes/opscenter.rb
+++ attributes/opscenter.rb
@@ -8,6 +8,7 @@ default['opscenter']['ssl_certfile'] = nil
 default['opscenter']['ssl_port'] = nil
 
 default['opscenter']['auth_enabled'] = 'False'
+default['opscenter']['authentication_method'] = 'DatastaxEnterpriseAuth'
 
 default['opscenter']['stat_reporter_interval'] = nil
 
@@ -15,3 +16,27 @@ default['opscenter']['stat_reporter_interval'] = nil
 # browser and opscenterd leading to 0 nodes showing
 # http://www.datastax.com/documentation/opscenter/5.1/opsc/troubleshooting/opscTroubleshootingZeroNodes.html
 default['opscenter']['use_longpoll'] = false
+
+default['opscenter']['ldap']['server_host'] = nil
+default['opscenter']['ldap']['server_port'] = nil
+default['opscenter']['ldap']['uri_scheme'] = nil
+default['opscenter']['ldap']['search_dn'] = nil
+default['opscenter']['ldap']['search_password'] = nil
+default['opscenter']['ldap']['user_search_base'] = nil
+default['opscenter']['ldap']['user_search_filter'] = nil
+default['opscenter']['ldap']['group_search_base'] = nil
+default['opscenter']['ldap']['group_search_filter'] = nil
+default['opscenter']['ldap']['group_search_filter_with_dn'] = nil
+default['opscenter']['ldap']['group_name_attribute'] = nil
+default['opscenter']['ldap']['admin_group_name'] = nil
+default['opscenter']['ldap']['user_memberof_attribute'] = nil
+default['opscenter']['ldap']['group_search_type'] = nil
+default['opscenter']['ldap']['ssl_cacert'] = nil
+default['opscenter']['ldap']['ssl_cert'] = nil
+default['opscenter']['ldap']['ssl_key'] = nil
+default['opscenter']['ldap']['tls_reqcert'] = nil
+default['opscenter']['ldap']['tls_demand'] = nil
+default['opscenter']['ldap']['ldap_security'] = nil
+default['opscenter']['ldap']['connection_timeout'] = nil
+default['opscenter']['ldap']['opt_referrals'] = nil
+default['opscenter']['ldap']['protocol_version'] = nil
