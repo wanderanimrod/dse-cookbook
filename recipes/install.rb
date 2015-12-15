4a2c14fcb29f88ed5d8f2106ce1e99bb209325df
Noorul Islam K M <noorul@noorul.com>
2015-12-15 15:21:25 +0530

    Code cleanup!
    1. Use better variable name.
    2. Factor out code from loop.

--- recipes/install.rb
+++ recipes/install.rb
@@ -27,15 +27,15 @@ end
 case node['platform']
 # make sure not to overwrite any conf files on upgrade
 when 'ubuntu', 'debian'
-  node['cassandra']['packages'].each do |install|
-    # We need to install datastax-agent upfront because dse package has it as an
-    # open dependency and therefore the specified datastax-agent version may not
-    # be fulfilled.
-    package 'datastax-agent' do
-      version node['datastax-agent']['version']
-      action :install
-    end
-    package install do
+  # We need to install datastax-agent upfront because dse package has it as an
+  # open dependency and therefore the specified datastax-agent version may not
+  # be fulfilled.
+  package 'datastax-agent' do
+    version node['datastax-agent']['version']
+    action :install
+  end
+  node['cassandra']['packages'].each do |pkg|
+    package pkg do
       version node['cassandra']['dse_version']
       action :install
       options '-o Dpkg::Options::="--force-confold"'
