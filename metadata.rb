c71be5a026826913a782379bb2055374ddef43bd
Giacomo Bagnoli <gbagnoli@etsy.com>
2016-09-01 11:24:49 +0100

    Add two more tunables for cassandra 3.x
    See http://www.datastax.com/dev/blog/token-allocation-algorithm for more
    info on allocate_tokens_for_keyspace setting

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'daniel.c.parker@target.com'
 license 'Apache 2.0'
 description 'Installs/Configures Datastax Enterprise.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.34'
+version '3.0.35'
 
 %w(redhat centos).each do |name|
   supports name, '>= 6.4'
